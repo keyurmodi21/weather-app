82fef7ea20fb842e973a3ed7d7974f6e7ab5fb0a
Keyur <km211089@gmail.com>
2020-03-02 22:16:26 +0800

    Updates

---
+++ precache-manifest.1f504c6845a43e52d6fa274efad24963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41dfd0edf6b16cacb76a3e5ddbe73d3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "7c2713173b59df86c3b8",
+    "url": "/weather-app/static/css/main.a4736182.chunk.css"
+  },
+  {
+    "revision": "8b5c4e493c9ed53a44c1",
+    "url": "/weather-app/static/js/2.166e24c3.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/weather-app/static/js/2.166e24c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2713173b59df86c3b8",
+    "url": "/weather-app/static/js/main.0e7adb22.chunk.js"
+  },
+  {
+    "revision": "f95d929b2dc853b2601a",
+    "url": "/weather-app/static/js/runtime-main.0c118c13.js"
+  },
+  {
+    "revision": "e93bff1adb4dc26a07206c8c34b9751d",
+    "url": "/weather-app/static/media/perth.e93bff1a.jpg"
+  }
+]);
